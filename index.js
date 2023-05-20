dbf6132e6583aab57cef4975c9088b9838fc1005
Hen <hen128@qq.com>
2023-05-20 20:39:58 +0800

    添加路由

--- index.js
+++ index.js
@@ -1,10 +1,23 @@
 const Koa = require('koa');
 const app = new Koa();
 
-app.use(async ctx => {
-    ctx.body = 'Hello Vercel, 新的接口2';
-});
-
+const router = require('koa-router')()
+router.get('/', (ctx)=>{
+    ctx.body = '接口';
+})
+router.get('/file', (ctx)=>{
+    ctx.body = 'file';
+})
+router.get('/user', (ctx)=>{
+    ctx.body = 'user';
+})
+router.post('/user', (ctx)=>{
+    ctx.body = 'POST user';
+})
+router.put('/user', (ctx)=>{
+    ctx.body = 'PUT user';
+})
+app.use(router.routes(), router.allowedMethods())
 
 app.listen(3008, () => {
     console.log('3008项目启动')
