d5f0d88adc78a9debfeee0bf586155083f49f434
Hen <hen128@qq.com>
2023-05-20 19:01:09 +0800

    vercel

--- app.js
+++ app.js
@@ -2,7 +2,9 @@ const Koa = require('koa');
 const app = new Koa();
 
 app.use(async ctx => {
-  ctx.body = 'Hello, World!';
+    ctx.body = 'Hello Vercel';
 });
 
-app.listen(3000);
+app.listen(3008, () => {
+    console.log('3008项目启动')
+});
